7fc1f1794f4952124398bbba8ad6b47a1007b6fe
Ben Scobie <benscobie@users.noreply.github.com>
2025-11-29 23:30:14 +0000

    Tidy up docs redirect

--- ui/src/pages/DocsPage.tsx
+++ ui/src/pages/DocsPage.tsx
@@ -1,12 +1,9 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 const DocsPage = () => {
-  const navigate = useNavigate()
-
   useEffect(() => {
     window.location.href = 'https://docs.maintainerr.info/latest/Introduction'
-  }, [navigate])
+  }, [])
 
   return <div className="text-white">Redirecting to documentation...</div>
 }
