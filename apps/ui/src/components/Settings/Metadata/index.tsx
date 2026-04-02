04e27e43679d31189a849676d2670d3cd6797b78
enoch85 <mailto@danielhansson.nu>
2026-04-02 20:03:01 +0000

    change tvdb link

--- apps/ui/src/components/Settings/Metadata/index.tsx
+++ apps/ui/src/components/Settings/Metadata/index.tsx
@@ -164,7 +164,7 @@ const providers: ProviderConfig[] = [
       <>
         You can create an API key at{' '}
         <a
-          href="https://thetvdb.com/dashboard/account/apikey"
+          href="https://thetvdb.com/api-information/signup"
           target="_blank"
           rel="noreferrer"
           className="text-amber-500 underline hover:text-amber-400"
