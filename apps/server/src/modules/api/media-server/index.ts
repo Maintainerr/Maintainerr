b7f4ed9737428d9ffc71f273502f250e5e5b7f9f
Craig Linford <craig.linford@sas.com>
2026-04-21 08:39:30 +0100

    fix(media-server): stop exporting plex internals

--- apps/server/src/modules/api/media-server/index.ts
+++ apps/server/src/modules/api/media-server/index.ts
@@ -2,5 +2,3 @@ export * from './media-server.module';
 export * from './media-server.interface';
 export * from './media-server.factory';
 export * from './media-server.constants';
-export * from './plex/plex-adapter.service';
-export * from './plex/plex.mapper';
